cab6d2c3762fc250ea5e304b37c5cc319d36ccc9
Dani Santillanes <danisantillanes@gmail.com>
2017-03-23 12:32:43 -0700

    Se cambia bs

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/ejemplogenerador'
+      || 'mongodb://admin:1234@ds139470.mlab.com:39470/api-generador'
   }
 };
